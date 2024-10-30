8979ad80cf07019c6b62981863a46bd80f797797
Sarthak-1203 <sarthaksri54@gmail.com>
2024-10-30 21:40:10 +0530

    Add files via upload

---
+++ Assets/data.js
@@ -0,0 +1,185 @@
+import p1_img from "./mask.png";
+import p2_img from "./oxy.png";
+import p3_img from "./bp.png";
+import p4_img from "./sugar.png";
+import p5_img from "./thermometer.png";
+import p6_img from "./volini.png";
+import p7_img from "./aspirin.png";
+import p8_img from "./dolo65.png";
+import p9_img from "./ORS.png";
+import p10_img from "./citralka.png";
+import p11_img from "./whisper.png";
+import p12_img from "./shelcal.png";
+import p13_img from "./preganews.png";
+import p14_img from "./multivitamin.png";
+import p15_img from "./horlicks.png";
+import p16_img from "./babywipes.png";
+import p17_img from "./baby rash.png";
+import p18_img from "./ddrops.png";
+import p19_img from "./pampers.png";
+import p20_img from "./horlickschild.png";
+
+let data_product=[
+    {
+        id:1,
+        name:"Premium N95 Mask For Men And Women",
+        category:"Medical Equipment",
+        image:p1_img,
+        new_price: 129,
+        old_price: 149,
+    },
+    {
+        id:2,
+        name:"Beurer PO30 Pulse Oximeter, Blood Oxygen Saturation & Heart Rate Monitor",
+        category:"Medical Equipment",
+        image:p2_img,
+        new_price: 999,
+        old_price: 1099,
+    },
+    {
+        id:3,
+        name:"Fully Automatic Digital Blood Pressure Monitor",
+        category:"Medical Equipment",
+        image:p3_img,
+        new_price: 1499,
+        old_price: 1599,
+    },
+    {
+        id:4,
+        name:"Simple & accurate testing of Blood sugar levels at home",
+        category:"Medical Equipment",
+        image:p4_img,
+        new_price: 659,
+        old_price: 859,
+    },
+    {
+        id:5,
+        name:"Digital Thermometer With Quick Measurement",
+        category:"Medical Equipment",
+        image:p5_img,
+        new_price: 139,
+        old_price: 179,
+    },
+    {
+        id:6,
+        name:"Volini Activ Spray 60 Gm",
+        category:"Medicine",
+        image:p6_img,
+        new_price: 289,
+        old_price: 299,
+    },
+    {
+        id:7,
+        name:"Disprin Regular 325mg - Strip of 10 Tablets",
+        category:"Medicine",
+        image:p7_img,
+        new_price: 100,
+        old_price: 109,
+    },
+    {
+        id:8,
+        name:"Dolo-650 - Strip of 15 Tablets",
+        category:"Medicine",
+        image:p8_img,
+        new_price: 25,
+        old_price: 29,
+    },
+    {
+        id:9,
+        name:"Oral Rehydration Powder- Pack of 21.80 g",
+        category:"Medicine",
+        image:p9_img,
+        new_price: 32,
+        old_price: 35,
+    },
+    {
+        id:10,
+        name:"Citralka - Bottle of 100 ml Syrup",
+        category:"Medicine",
+        image:p10_img,
+        new_price: 89,
+        old_price: 99,
+    },
+    {
+        id:11,
+        name:"WHISPER ULTRA HYGIENE+COMFORT SANITARY PADS",
+        category:"Women care",
+        image:p11_img,
+        new_price: 110,
+        old_price: 119,
+    },
+    {
+        id:12,
+        name:"Shelcal-500 - Strip of 15 Tablets",
+        category:"Women care",
+        image:p12_img,
+        new_price: 139,
+        old_price: 149,
+    },
+    {
+        id:13,
+        name:"Prega News Test Kit One Step Urine HCG Pregnancy Test Kit Device",
+        category:"Women care",
+        image:p13_img,
+        new_price: 59,
+        old_price: 69,
+    },
+    {
+        id:14,
+        name:"Centrum Women, World's No.1 Multivitamin",
+        category:"Women care",
+        image:p14_img,
+        new_price: 666,
+        old_price: 740,
+    },
+    {
+        id:15,
+        name:"Horlicks Women's Plus Chocolate Nutrition Drink 400 g",
+        category:"Women care",
+        image:p15_img,
+        new_price: 289,
+        old_price: 356,
+    },
+    {
+        id:16,
+        name:"Johnson's Baby Wet Wipes",
+        category:"Baby care",
+        image:p16_img,
+        new_price: 280,
+        old_price: 291,
+    },
+    {
+        id:17,
+        name:"Himalaya Baby Diaper Rash Cream (100g)",
+        category:"Baby care",
+        image:p17_img,
+        new_price: 159,
+        old_price: 220,
+    },
+    {
+        id:18,
+        name:"Ddrops Vit D Baby 400 IU 90 Drop ",
+        category:"Baby care",
+        image:p18_img,
+        new_price: 3654,
+        old_price: 5849,
+    },
+    {
+        id:19,
+        name:"Pampers All round Protection Pants, baby diapers",
+        category:"Baby care",
+        image:p19_img,
+        new_price: 910,
+        old_price: 1149,
+    },
+    {
+        id:20,
+        name:"Cadbury Bournvita Chocolate Nutrition",
+        category:"Baby care",
+        image:p20_img,
+        new_price: 385,
+        old_price: 458,
+    },
+
+];
+export default data_product;
